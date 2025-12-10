a7adaa8e6f0c93f20f369cc2dbc2eb5504c68448
Kael Guerrero <kaelguerrero22@gmail.com>
2025-12-10 10:48:36 -0500

    firstbenchmark

---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template
+
+import pymysql
+import pymysql.cursors
+
+from dynaconf import Dynaconf
+
+
+app = Flask(__name__)
+
+config = Dynaconf(settings_file=["settings.toml"])
+
+def connect_db():
+    conn = pymysql.connect(
+        host="db.steamcenter.tech",
+        user="kguerrero",
+        password=config.password,
+        database="kguerrero_bready_store",
+        autocommit=True,
+        cursorclass=pymysql.cursors.DictCursor
+    )
+
+    return conn
+
+@app.route("/")
+def index():
+    return render_template("homepage.html.jinja")
+
+@app.route("/browse")
+def browse():
+    connection = connect_db()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM `Product`")
+    result = cursor.fetchall()
+    connection.close()
+    return render_template("browse.html.jinja", products = result)
